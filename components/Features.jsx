ab8aa60bdd16e0de3037c4048b6a56b47fda2e76
VladanIlicJS <vladanilic017@gmail.com>
2023-04-18 21:19:49 +0200

    Features images added

--- components/Features.jsx
+++ components/Features.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
-
+import profile from "../public/assets/icon-person.png"
+import money from '../public/assets/icon-money.png'
+import growth from '../public/assets/icon-growth.png'
 const Features = () => {
   return (
     <div className=' pt-[75px] w-full bg-[#fbfbff]'>
@@ -12,7 +14,7 @@ const Features = () => {
         <div className='py-20 px-2 2xl:px-40 grid grid-cols-3'>
             <div className='grid grid-cols-[1fr,4fr]'>
                 <div>
-                    <Image src="/../public/assets/icon-person.png"  width={69} height={69} alt="/" />
+                    <Image src={profile}  width={69} height={69} alt="/" />
                 </div>
                 <div className='px-7'>
                     <h5 className=' leading-[30px] text-[#2b2d80] text-[18px] font-[600]'>Efficient Fleet Use</h5>
@@ -21,7 +23,7 @@ const Features = () => {
             </div>
             <div className='grid grid-cols-[1fr,4fr]'>
                 <div>
-                    <Image  className='' src="/../public/assets/icon-money.png" width={69} height={69} alt="/" />
+                    <Image  className='' src={money} width={69} height={69} alt="/" />
                 </div>
                 <div className='px-5'>
                     <h5 className=' leading-[30px] text-[#2b2d80] text-[18px] font-[600]'>Lower Cost</h5>
@@ -30,7 +32,7 @@ const Features = () => {
             </div>
             <div className='grid grid-cols-[1fr,4fr]'>
                 <div>
-                    <Image  className='' src="/../public/assets/icon-growth.png" width={69} height={69} alt="/" />
+                    <Image  className='' src={growth} width={69} height={69} alt="/" />
                 </div>
                 <div className='px-5'>
                     <h5 className=' leading-[30px] text-[#2b2d80] text-[18px] font-[600]'>Faster Growth</h5>
