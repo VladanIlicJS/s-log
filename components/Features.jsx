d5f9fd8568fd85408bf51dfc844db74f9440588b
VladanIlicJS <vladanilic017@gmail.com>
2023-04-22 23:34:03 +0200

    features section responsive

--- components/Features.jsx
+++ components/Features.jsx
@@ -11,30 +11,30 @@ const Features = () => {
             <p className='text-[#a1a5ff] mb-3'>Easy to use route planning solution</p>
             <h2 className='text-2xl font-[500]  text-[#2b2d80]'>Route planning and optimization for top results</h2>
         </div>
-        <div className='py-20 px-2 2xl:px-40 grid grid-cols-3'>
-            <div className='grid grid-cols-[1fr,4fr]'>
+        <div className='py-20 px-2 2xl:px-40 grid lg:grid-cols-3 grid-cols-1 items-center justify-center lg:gap-0 gap-5'>
+            <div className='w-full lg:grid lg:grid-cols-[1fr,4fr] flex justify-center flex-col items-center'>
                 <div>
                     <Image src={profile}  width={69} height={69} alt="/" />
                 </div>
-                <div className='px-7'>
+                <div className='px-7 lg:text-start text-center'>
                     <h5 className=' leading-[30px] text-[#2b2d80] text-[18px] font-[600]'>Efficient Fleet Use</h5>
                     <p className='text-[#343d48] leading-7 text-[15px]'>Maximize performance of your fleet based on vehicle capacity, profit, and time limitation. </p>
                 </div>
             </div>
-            <div className='grid grid-cols-[1fr,4fr]'>
+            <div className='w-full lg:grid lg:grid-cols-[1fr,4fr] flex justify-center flex-col items-center'>
                 <div>
                     <Image  className='' src={money} width={69} height={69} alt="/" />
                 </div>
-                <div className='px-5'>
+                <div className='px-7 lg:text-start text-center'>
                     <h5 className=' leading-[30px] text-[#2b2d80] text-[18px] font-[600]'>Lower Cost</h5>
                     <p className='text-[#343d48] leading-7 text-[15px]'>Minimize costs by using your resources in the most optimal manner. </p>
                 </div>
             </div>
-            <div className='grid grid-cols-[1fr,4fr]'>
+            <div className='w-full lg:grid lg:grid-cols-[1fr,4fr] flex justify-center flex-col items-center'>
                 <div>
                     <Image  className='' src={growth} width={69} height={69} alt="/" />
                 </div>
-                <div className='px-5'>
+                <div className='px-7 lg:text-start text-center'>
                     <h5 className=' leading-[30px] text-[#2b2d80] text-[18px] font-[600]'>Faster Growth</h5>
                     <p className='text-[#343d48] leading-7 text-[15px]'>Increase profit and grow your transportation business faster. </p>
                 </div>
