c2eabc759bbf9763f51f0696a3ada989d6d492d8
VladanIlicJS <vladanilic017@gmail.com>
2023-04-23 00:21:47 +0200

    Two more

--- components/RoutePlanner.jsx
+++ components/RoutePlanner.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import {AiOutlinePlus} from "react-icons/ai"
-import CollapsibleItems from "./Dropdowns";
+import React from "react"
+import { AiOutlinePlus } from "react-icons/ai"
+import CollapsibleItems from "./Dropdowns"
+import planner from "../public/assets/smart-route-planner.png"
+import Image from "next/image"
 const RoutePlanner = () => {
   return (
-    <div className="w-full pt-20">
-      <div className=" relative bg-planner w-full h-[600px] bg-no-repeat bg-[length:55%_100%]">
-        <div className="absolute grid grid-cols-[10fr,7fr]">
-          <div></div>
-          <div className="py-3 px-3 pr-40">
+    <div className="w-full pt-20 pb-1 mb-1">
+      <div className="  lg:bg-planner w-full lg:h-[600px] bg-no-repeat bg-[length:55%_100%]">
+        <div className=" grid lg:grid-cols-[10fr,7fr] grid-cols-1">
+          <div className="lg:block hidden"></div>
+          <div className="py-3 2xl:px-3 2xl:pr-40 px-9 lg:text-start text-center w-full">
             <h5 className="text-[#a1a5ff] text-[16px]">Smart Route Planner</h5>
             <h1 className="text-[#2b2d80] text-3xl leading-[40px] pt-[32px] font-[500] ">
-              <span className="text-[#a1a5ff]">SLog</span> makes route planning
-              easier, faster and efficient
+              <span className="text-[#a1a5ff]">SLog</span> makes route planning easier, faster and efficient
             </h1>
-            <p className="leading-7 pt-[12px] pr-20">
-              Our team will set up the software to optimize for your unique
-              business structure and goals.
-            </p>
+            <p className="leading-7 pt-[12px]">Our team will set up the software to optimize</p>
+            <p className="leading-7">for your unique business structure and goals.</p>
             <div>
-            <CollapsibleItems/>
+              <CollapsibleItems />
               {/* <div className="py-[20px] px-[30px] shadow-md rounded-md">
                 <div className="flex items-center justify-between text-[#a1a5ff] py-[20px]">
                   <h3 className="text-[#a1a5ff] text-[15px]">Shorter routes</h3>
@@ -36,10 +35,13 @@ const RoutePlanner = () => {
               </div> */}
             </div>
           </div>
+          <div className="w-full lg:hidden">
+            <Image src={planner} fill sizes="(max-width: 768px) 100vw" />
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default RoutePlanner;
+export default RoutePlanner
